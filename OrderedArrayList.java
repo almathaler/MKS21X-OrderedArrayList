cd29bccf555eaaed2d24956fedee57140b78fdd4
almathaler <athaler00@stuy.edu>
2018-11-01 14:49:19 -0400

    did noNull, remember to delete redundant elses

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -9,12 +9,28 @@ public class NoNullArrayList<T> extends ArrayList<T> {
 
 	public T set(int i, T value) {
 		if (value == null) {
-			throw new IllegalArguementException("Error: can't add null to NoNullArrayList");
+			throw new IllegalArguementException("Error: can't set element to null in NoNullArrayList");
 		}
 		else {
 			return super.set(i, value);
 		}
 	}
 
-	
+	public boolean add(T value) {
+		if (value == null) {
+			throw new IllegalArguementException("Error: can't add null value to NoNullArrayList");		
+		}
+		else {
+			return super.add(value);	
+		}
+	}	
+
+	public void add(int i, T value) {
+		if (value == null) {
+			throw new IllegalArguementException("Error: can't add null value to NoNullArrayList");			
+		}
+		else {
+			super.add(i, value);
+		}
+	}
 }
