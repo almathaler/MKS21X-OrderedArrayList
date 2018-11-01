ca164a09eaa095bd3ece0db63be80ef4352537d6
almathaler <athaler00@stuy.edu>
2018-11-01 18:23:41 -0400

    renamed file

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,36 +1,30 @@
 import java.util.ArrayList;
 public class NoNullArrayList<T> extends ArrayList<T> {
 	public NoNullArrayList() {
-		super();	
+		super();
 	}
 	public NoNullArrayList(int startingCapacity) {
-		super(startingCapacity);	
+		super(startingCapacity);
 	}
 
 	public T set(int i, T value) {
 		if (value == null) {
 			throw new IllegalArguementException("Error: can't set element to null in NoNullArrayList");
 		}
-		else {
-			return super.set(i, value);
-		}
+		return super.set(i, value);
 	}
 
 	public boolean add(T value) {
 		if (value == null) {
-			throw new IllegalArguementException("Error: can't add null value to NoNullArrayList");		
+			throw new IllegalArguementException("Error: can't add null value to NoNullArrayList");
 		}
-		else {
-			return super.add(value);	
-		}
-	}	
+		return super.add(value);
+	}
 
 	public void add(int i, T value) {
 		if (value == null) {
-			throw new IllegalArguementException("Error: can't add null value to NoNullArrayList");			
+			throw new IllegalArguementException("Error: can't add null value to NoNullArrayList");
 		}
-		else {
-			super.add(i, value);
-		}
+		super.add(i, value);
 	}
 }
