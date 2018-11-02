ba1dba351d3defccb98a51f9710f8b53efd8f18e
almathaler <athaler00@stuy.edu>
2018-11-02 14:47:07 -0400

    Added faulty OrderedArrayList

---
+++ OrderedArrayList.java
@@ -0,0 +1,38 @@
+OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T> {
+	public OrderedArrayList() {
+		super();	
+	}
+	public OrderedArrayList(int startingCapacity) {
+		super(startingCapacity);
+	}
+`	public int findIndex(T element) {
+		int foundIndex = 0;
+		if (this.size() == 0) {
+			return foundIndex;
+		}
+		for (int i = 0; i<this.size(); i++) {
+			if (element.compareTo(this.get(i)) == 0) {
+				return foundIndex;
+			}
+			foundIndex = i;
+		}
+		return foundIndex;
+	}
+	public boolean add(T element) {
+		index = findIndex(element);
+		super.add(index, element);
+		return true;
+	}
+	public void add(int index, T element) {
+		i = findIndex(element);
+		super.add(i, element);
+	}
+	public T set(int index, T element) {
+		remove(element);
+		add(element);
+	}
+	//for adding, instance.compareTo() returns -1 if instance is before parameter (can be like index += instance.compareTo(). it returns 0 if they're
+	//lexicographically equivalent (so it can be put in place of the parameter or after, it's whatever), and +1 if it's after (index += instance.compareTo())
+	//probably same with ints
+	//use super.add(int i, T element) after you find the proper index to put the element at so it shifts all the elements over to make space
+}
